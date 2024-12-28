1505badeddd67f72ff87012a8ceb488710851d8a
vikasrath <vikasrathote01322@gmail.com>
2024-12-27 23:11:11 -0800

    cors added

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ import express from "express";
 import dotenv from "dotenv"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { page } from "./controllers/pages.controller.js";
+import cors from "cors";
 
 dotenv.config()
 
 const app = express()
 const Port = process.env.PORT;
 
+app.use(cors({
+    origin: "https://solocorp-frontend.vercel.app",
+    methods: ["GET"],
+}));
 app.use(express.json());
 
 app.get("/data/:page", page);
