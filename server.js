be0df1c0d22b6fc8674d3e6d96427de5e38c1954
vikasrath <vikasrathote01322@gmail.com>
2024-12-28 23:59:52 -0800

    SSR DONE

--- server.js
+++ server.js
@@ -1,23 +1,51 @@
-import express from "express";
-import dotenv from "dotenv"
-import connectToMongoDB from "./db/connectToMongoDB.js";
-import { page } from "./controllers/pages.controller.js";
-import cors from "cors";
+import express from 'express';
+import path from 'path';
+import { renderToString } from 'react-dom/server';
+import React from 'react';
+import { StaticRouter } from 'react-router-dom/server';
+import App from './src/App';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import {page} from "./controllers/pages.controller"
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const Port = process.env.PORT;
+const app = express();
+const Port = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 app.use(cors({
     origin: "https://solocorp-frontend.vercel.app",
     methods: ["GET"],
 }));
+app.use(express.static(path.join(__dirname, "frontend/dist")));
 app.use(express.json());
 
-app.get("/data/:page", page);
+app.get("/api/data/:page", page);
 
-app.listen(Port, ()=> {
+app.get("*", (req, res) => {
+    const context = {};
+    const appString = renderToString(
+        <StaticRouter location={req.url} context={context}>
+            <App />
+        </StaticRouter>
+    );
+
+    res.send(`
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>SSR with React</title>
+                <script src="/bundle.js" defer></script>
+            </head>
+            <body>
+                <div id="root">${appString}</div>
+            </body>
+        </html>
+    `);
+});
+
+app.listen(Port, () => {
     console.log(`Server is working on port ${Port}`);
-    connectToMongoDB()
-})
+    connectToMongoDB(); 
+});
